6638f4beb714b97f02245ded7cc0d6de81510d89
wanggx <wgxdyx.c2c@gmail.com>
2016-03-17 21:53:18 +0800

    代码初始化

---
+++ Linux2.6.11/Linux2.6.11/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Linux2.6.11
+//
+//  Created by gxwang on 16/3/17.
+//  Copyright © 2016年 gxwang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
